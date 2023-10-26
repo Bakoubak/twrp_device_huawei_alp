1e318fa0f84f5fca350cacef3741e064038c2440
VistaSlayer <Bakoubak85@gmail.com>
2023-10-26 12:47:24 +0200

    Initial Commit

---
+++ BoardConfig.mk
@@ -0,0 +1,57 @@
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := KIRIN
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := generic
+TARGET_BOARD_PLATFORM_GPU := kirin
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_SMP := true
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+
+BOARD_KERNEL_CMDLINE := 
+
+BOARD_KERNEL_BASE := 0x10000000
+BOARD_KERNEL_PAGESIZE := 2048
+BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x8000 --ramdisk_offset 0x01000000 --tags_offset 0x0100
+
+# Use a dummy kernel since its going to be flashed to the recovery_ramdisk anyway and will have no use.
+TARGET_PREBUILT_KERNEL := device/huawei/alp/dummykernel
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 33554432
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 5905580032
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 119663493120
+BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
+BOARD_FLASH_BLOCK_SIZE := 131072 # (BOARD_KERNEL_PAGESIZE * 64)
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
+
+TARGET_USERIMAGES_USE_EXT4 := true
+TARGET_USERIMAGES_USE_F2FS := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+
+TW_THEME := portrait_hdpi
+BOARD_SUPPRESS_SECURE_ERASE := true
+RECOVERY_SDCARD_ON_DATA := true
+TW_EXCLUDE_DEFAULT_USB_INIT := true
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd_backlight0/brightness
+TW_NO_HAPTICS := true
+TW_NO_SCREEN_BLANK := true
+TW_USE_TOOLBOX := true
+TW_DEFAULT_BRIGHTNESS := "2048"
+TW_CUSTOM_BATTERY_PATH := /sys/class/power_supply/Battery
+# Device crashes if /sbin/modprobe is present so this is needed:
+BOARD_CUSTOM_BOOTIMG_MK := device/huawei/alp/custombootimg.mk
+# MTP will not work until we update it to support ffs
+TW_EXCLUDE_MTP := true
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # twrp_device_huawei_alp
  TWRP Device tree for Huawei Mate 10 on EMUI 9.1
+ This Device tree is based on the P20 Pro which shares similar specs with the Mate 10.
+ 
+ Credits to TWRP Team, and everyone who worked on the P20 Pro and Mate 10 Pro tree. 
